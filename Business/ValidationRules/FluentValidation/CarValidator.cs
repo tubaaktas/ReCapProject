fd3872dfeb053ff148e39d75c8707fe6d964ad7c
tubaaktas <tubaaktas83@gmail.com>
2021-02-20 15:39:24 +0300

    Fluent Validation desteği eklendi.

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.CarName).MinimumLength(2);
+            RuleFor(c => c.CarName).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(1300).When(c => c.BrandId == 2).WithMessage("The daily price limit for BrandId = 2 is at least 1300... ");
+            //açıklama kısmı V ile başlamalı, V6, V8,V4...
+            RuleFor(c => c.Description).Must(StartWithV).WithMessage("Description must start with 'V'");
+
+
+        }
+
+        private bool StartWithV(string arg)
+        {
+            return arg.StartsWith("V");
+        }
+    }
+
+
+}
---
+++ Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RentalValidator:AbstractValidator<RentalValidator>
+    {
+        public RentalValidator()
+        {
+
+        }
+    }
+}
